56263ae72cc9a85cd0a538b2d5ff65006f6642fc
gmpm292 <gmpm292@gmail.com>
2025-03-17 18:41:56 -0400

    LogOut

--- src/layout/components/MainLayout.jsx
+++ src/layout/components/MainLayout.jsx
@@ -1,16 +1,16 @@
-import { Card } from 'primereact/card';
-import { Sidebar } from './Sidebar';
-import '../styles/MainLayout.css';
+import { Card } from "primereact/card";
+import { Sidebar } from "./Sidebar";
+import { TopBar } from "./TopBar";
+import "../styles/MainLayout.css";
 
 export function MainLayout({ children }) {
-    return (
-        <div className="layout-wrapper">
-            <Sidebar />
-            <div className="layout-main">
-                <Card className="layout-content">
-                    {children}
-                </Card>
-            </div>
-        </div>
-    );
-} 
+  return (
+    <div className="layout-wrapper">
+      <TopBar />
+      <Sidebar />
+      <div className="layout-main">
+        <Card className="layout-content">{children}</Card>
+      </div>
+    </div>
+  );
+}
--- src/modules/auth/graphql/queries.js
+++ src/modules/auth/graphql/queries.js
@@ -1,6 +1,6 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
-export const CLASSIC_LOGIN = gql`
+const CLASSIC_LOGIN = gql`
   query classicLogin($input: ClassicLoginInput!) {
     classicLogin(input: $input) {
       profile {
@@ -10,4 +10,156 @@ export const CLASSIC_LOGIN = gql`
       }
     }
   }
-`; 
+`;
+
+const GET_PROFILE = gql`
+  query Profile {
+    profile {
+      id
+      email
+      enabled
+      name
+      lastName
+      mobile
+      zoomExt
+      role
+      office {
+        id
+        officeType
+        name
+      }
+      department {
+        id
+        departmentType
+        name
+      }
+      team {
+        id
+        teamType
+      }
+      isTwoFactorEnabled
+      isTwoFactorConfigured
+    }
+  }
+`;
+
+const REFRESH_TOKEN = gql`
+  mutation Refresh {
+    refresh {
+      accessToken
+    }
+  }
+`;
+
+const CHANGE_PASSWORD = gql`
+  mutation ChangePassword($input: ChangePasswordInput!) {
+    changePassword(changePasswordInput: $input) {
+      id
+    }
+  }
+`;
+
+const CHECK_CONFIRMATION_TOKEN = gql`
+  mutation CheckConfirmationToken($input: CheckConfirmationTokenInput!) {
+    checkConfirmationToken(checkConfirmationTokenInput: $input)
+  }
+`;
+
+const REQUEST_PASSWORD_CHANGE_FOR_ANOTHER_USER = gql`
+  mutation RequestPasswordChangeForAnotherUser(
+    $input: RequestPasswordChangeInput!
+  ) {
+    requestPasswordChangeForAnotherUser(input: $input) {
+      message
+    }
+  }
+`;
+
+const GENERATE_2FA_SECRET = gql`
+  mutation Generate2faSecret {
+    generate2faSecret
+  }
+`;
+
+const VERIFY_2FA = gql`
+  query Verify2FA($token2Fa: String) {
+    verify2FA(token2fa: $token2Fa) {
+      accessToken
+      refreshToken
+      profile {
+        id
+        email
+        enabled
+        name
+        lastName
+        mobile
+        zoomExt
+        role
+        office {
+          id
+          officeType
+          name
+        }
+        department {
+          id
+          departmentType
+          name
+        }
+        team {
+          id
+          teamType
+        }
+        isTwoFactorEnabled
+        isTwoFactorConfigured
+      }
+    }
+  }
+`;
+
+const FINISH_CONFIGURE_2FA = gql`
+  mutation FinishConfigure2FA($token2Fa: String) {
+    finishConfigure2FA(token2fa: $token2Fa)
+  }
+`;
+
+const LOGOUT = gql`
+  mutation Logout {
+    logout {
+      id
+    }
+  }
+`;
+
+const RESET_2FA_SETTINGS = gql`
+  mutation Reset2FASettings($id: Int) {
+    reset2FASettings(id: $id)
+  }
+`;
+
+const ENABLE_2FA = gql`
+  mutation Enable2FA($id: Int) {
+    enable2FA(id: $id)
+  }
+`;
+
+const DISABLE_2FA = gql`
+  mutation Disable2FA($id: Int) {
+    disable2FA(id: $id)
+  }
+`;
+
+export {
+  CLASSIC_LOGIN,
+  GET_PROFILE,
+  REFRESH_TOKEN,
+  CHANGE_PASSWORD,
+  CHECK_CONFIRMATION_TOKEN,
+  REQUEST_PASSWORD_CHANGE_FOR_ANOTHER_USER,
+  GENERATE_2FA_SECRET,
+  VERIFY_2FA,
+  FINISH_CONFIGURE_2FA,
+  LOGOUT,
+  RESET_2FA_SETTINGS,
+  ENABLE_2FA,
+  DISABLE_2FA,
+};
