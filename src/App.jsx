c18505346cd03334a1d0f37bf8577051186a830a
gmpm292 <gmpm292@gmail.com>
2025-10-16 15:10:10 -0400

    qztray

--- src/App.jsx
+++ src/App.jsx
@@ -38,8 +38,6 @@ import "./styles/ButtonStyles.css";
 import "./styles/dialogs.css";
 import "./components/BaseTable/styles.css";
 
-import { QZTrayLoader } from "./modules/printing/components/QZTrayLoader";
-
 // Importar locale español
 import { PrimeReactProvider } from "primereact/api";
 import { setupLocales } from "./locales/i18n";
@@ -71,7 +69,6 @@ function App() {
     <ApolloProvider client={client}>
       <PrimeReactProvider>
         <BrowserRouter>
-          <QZTrayLoader />
           <AuthProvider>
             <Routes>
               <Route path="/login" element={<LoginPage />} />
